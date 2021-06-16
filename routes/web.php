3272e040ad70cdfe2b1b3811cea81f6d1db3622d
Zubby <zubbybrightson@gmail.com>
2021-06-16 15:11:20 +0100

    questions view should be landing page

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/questions', function () {
     return view('questions');
 });
 
